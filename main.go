3596d42f94f94b5f6d47949f693b9c0fe1c0dddd
Hunter Howell <uncommonlisper@proton.me>
2025-12-10 20:47:36 -0500

    add database functionality and handle get and post request to /blogs by rendering templates

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+
+	"gorm.io/driver/sqlite"
+	_ "gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Blog struct {
+	ID      uint   `json:"id" gorm:"primary_key"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type CreateBlogInput struct {
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
+}
+
+var DB *gorm.DB
+
+func connectDatabase() {
+	database, err := gorm.Open(sqlite.Open("blogo.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = database.AutoMigrate(&Blog{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = database
+}
+
+func getBlogs(w http.ResponseWriter, r *http.Request) {
+	var blogs []Blog
+
+	DB.Find(&blogs)
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, blogs)
+}
+
+func createBlog(w http.ResponseWriter, r *http.Request) {
+	var blog Blog
+
+	json.NewDecoder(r.Body).Decode(&blog)
+
+	DB.Create(&blog)
+}
 
 func main() {
-	fmt.Println("hello world")
+	mux := http.NewServeMux()
+
+	connectDatabase()
+
+	mux.HandleFunc("GET /blogs", getBlogs)
+	mux.HandleFunc("POST /blogs", createBlog)
+
+	log.Print("Listening...")
+
+	http.ListenAndServe(":8080", mux)
 }
